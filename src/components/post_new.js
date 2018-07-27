20e0ff5218eb31522b12184920e40370a0273b32
David Cheng <DavidCheng@Davids-MacBook-Pro-2.local>
2018-07-27 13:02:57 -0700

    Blog redux starter file

---
+++ src/components/post_new.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
+
+
+class PostNew extends Component {
+  renderField(field) {
+    return (
+      <div className='form-group'>
+        <label>{field.label}</label>
+        <input 
+          className='form-control'
+          type='text'
+          {...field.input}
+        />
+        {field.meta.error}
+      </div>
+    );
+  } 
+
+ 
+  render() {
+    return (
+      <div>
+        <form>
+          <Field 
+            label='Title For Post'
+            name='title'
+            component={this.renderField}
+          />
+          <Field 
+            label='Categories'
+            name='categories'
+            component={this.renderField}
+          />
+          <Field
+            label='Content'
+            name='content'
+            component={this.renderField}
+          />
+          <button type="submit" className='btn btn-primary'>Submit</button>
+        </form>
+      </div>
+    )
+  };
+}
+
+function validate(values) {
+  const errors = {};
+
+  // Validate the inputs from 'values'
+  if (!values.title) {
+    errors.title = 'Enter a title!';
+  }
+  if (!values.categories) {
+    errors.categories = 'Enter some categories';
+  }
+  if (!values.content) {
+    errors.content = 'Enter some content please';
+  }
+  // If errors is empty, the form is fine to submit
+  // If errors has any properties, redux form assumes form is invalid
+  return errors;
+}
+
+export default reduxForm({
+  validate,
+  form: 'PostsNewForm'
+
+})(PostNew);
